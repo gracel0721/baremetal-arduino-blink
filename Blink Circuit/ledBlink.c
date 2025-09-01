09796f3beba783eb76292feb1d6b3a1750621a45
gracel0721 <gvleverett@gmail.com>
2025-09-01 17:08:38 -0400

    add blink circuit

---
+++ Blink Circuit/ledBlink.c
@@ -0,0 +1,39 @@
+#define F_CPU 16000000UL
+
+#include <util/delay.h>
+#include <avr/io.h>
+
+int main(void){
+    // Port b pins = 8-13
+    // port c = analog pins
+    // port d = pins 0-7
+
+    // PORT sets pin to high/low, read/write
+    // DDR is data direction register - sets pins as input or output, read/write
+    // PIN is input read only
+
+// Default clock source is internal 8MHz RC oscillator
+
+
+//Set pin 13 (built in led) as output
+    DDRD |= (1 << DDD6);
+
+    while (1)
+    {
+        //set pin 13 high
+        PORTD |= (1 << PORTD6);
+        _delay_ms(1000);
+
+        //set pin 13 low
+        PORTD &= ~(1 << PORTD6);
+        _delay_ms(1000);
+    }
+    return 0;
+}
+
+    
+
+
+
+
+
--- README.md
+++ README.md
@@ -3,4 +3,7 @@ Arduino Blink in Baremetal raw C programming
 
 Run ```make``` in the command line to run the makefile, then ```make deploy``` to deploy the code to your arduino
 
+The "Classic Blink" makes the built in led blink. 
+The "blink circuit" makes an external led (hooked up to Pin 6) blink. 
+
 Note: I couldn't get the makefile to work in the VSCode command line, but If i run the commands indivivually in the command line terminal, this program works
